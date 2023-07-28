ffdf88a0cb3ddfabcdd3816f711ed448e6a03ed2
Neal Wang <nealwang.sh@protonmail.com>
2023-07-28 15:19:39 -0700

    still doesnt work

--- USACO/1302_e.cpp
+++ USACO/1302_e.cpp
@@ -1,50 +1,66 @@
 #include <iostream>
-#include <tuple>
 #include <vector>
+
+struct Customer {
+	int a, b, c;
+};
+
+int N, TC, TM;
+std::vector<Customer> customers;
+
+bool is_possible(int x) {
+	for (int i = 0; i <= x; ++i) {
+		int tc = TC - i;
+		int tm = TM - (x - i);
+
+		if (tc <= 0 || tm <= 0)
+			continue;
+
+		bool sad = 0;
+		for (Customer c : customers) {
+			if (c.a * tc + c.b * tm > c.c)
+				sad = 1;
+		}
+
+		if (!sad)
+			return 1;
+	}
+
+	return 0;
+}
+
+void solve() {
+	std::cin >> N >> TC >> TM;
+
+	for (int i = 0; i < N; ++i) {
+		int a, b, c;
+		std::cin >> a >> b >> c;
+
+		customers.push_back({a, b, c});
+	}
+
+	int low = 0, high = TC + TM - 2;
+	while (low < high) {
+		int mid = (low + high) / 2;
+
+		if (is_possible(mid))
+			high = mid;
+		else
+			low = mid + 1;
+	}
+
+	std::cout << low << std::endl;
+}
 
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	int t;
-	std::cin >> t;
+	int T;
+	std::cin >> T;
 
-	while (t--) {
-		int n, tc, tm;
-		std::cin >> n >> tc >> tm;
-
-		std::vector<std::tuple<long long, long long, long long>> orders(
-		    n);
-
-		for (int i = 0; i < n; ++i) {
-			long long a, b, c;
-			std::cin >> a >> b >> c;
-
-			orders[i] = {a, b, c};
-		}
-
-		for (int s = 0; s <= tc + tm; ++s) {
-			for (int i = 0; i <= s; ++i) {
-				long long newTc = tc - i, newTm = tm - (s - i);
-				if (newTc < 1 || newTm < 1)
-					continue;
-
-				for (int i = 0; i < n; ++i) {
-					if (std::get<0>(orders[i]) * newTc +
-						std::get<1>(orders[i]) * newTm >
-					    std::get<2>(orders[i]))
-						goto skip;
-				}
-
-				std::cout << s << std::endl;
-				goto end;
-
-			skip:;
-			}
-		}
-
-	end:;
-	}
+	while (T--)
+		solve();
 
 	return 0;
 }
